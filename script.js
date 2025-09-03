079affc62228ec01f13b2cfb454de09a8ea6a3c9
milamesmo <camilaabegg.n@gmail.com>
2025-09-02 23:21:34 -0300

    add depoiment card slider

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const overlay = document.getElementById('overlay');
 const iconOpen = document.getElementById('menu__open'); 
 const iconClose = document.getElementById('menu__close');
 
+const cards = document.querySelectorAll(".depoiment__card");
+let indice = 0;
+const ultimoIndice = cards.length - 1;
+
+const dots = document.querySelectorAll('.depoiment__index');
+
+
+
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
@@ -23,3 +31,29 @@ overlay.addEventListener('click', () => {
   iconOpen.style.display = 'block';
   iconClose.style.display = 'none';
 });
+
+
+cards[indice].style.display = "block";
+dots[indice].style.backgroundColor = 'var(--BrightRed)';
+
+function mudaSlide(){
+  //esconder todos os slides
+  cards.forEach(function(card){
+    card.style.display = 'none';
+  })
+
+  dots.forEach(function(dot){
+    dot.style.backgroundColor = 'white';
+  })
+
+  if (indice>= ultimoIndice){
+    indice = -1;
+
+  }
+  //mostrar próximo slide e mudar indice
+  
+  cards[++indice].style.display = 'block';
+  dots[indice].style.backgroundColor = 'var(--BrightRed)';
+  
+}
+setInterval(mudaSlide, 3000);
